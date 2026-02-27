24bc09fd19386e8630f19a5b3d4303383bac1f99
dendroica <galaxycoff@gmail.com>
2026-02-27 15:17:40 -0500

    making new branch

--- LINDY/spp_run.R
+++ LINDY/spp_run.R
@@ -1,6 +1,6 @@
 usr <- "jgorzo"
-#mypath <- "C:/Users/galax/Downloads/SASLoads/SASLoads"
-mypath <- "V:/Marine Fish/Lindy/OceanTrawl/SASLoads"
+#input <- "C:/Users/galax/Downloads/SASLoads/SASLoads"
+input <- "V:/Marine Fish/Lindy/OceanTrawl/SASLoads"
 mywd <- file.path("C:/Users", usr, "Documents/njdep/LINDY")
 setwd(mywd)
 #source("OCEAN TRAWL ABUNDANCE WGTED-ARIM.R")
@@ -9,26 +9,26 @@ source("WGTED-ARIM.R")
 source("LFD WGTED.R")
 outdir <- file.path("C:/Users", usr,
                     "OneDrive - New Jersey Office of Information Technology/Documents/output/sasr")
-#sppdata <- Spp(mypath, "Black drum", "INM", "Oct", outdir="~/output/sasr")
-#sppdata <- Spp(mypath, "Scup", outdir=outdir) #"INM", "AugOct"
-#sppdata <- Spp(mypath, spp="Spot", area="INM", cruise="AugOct", outdir=outdir)
-#sppdata <- Spp(mypath, "Summer flounder", outdir="~/output/sasr")
-#sppdata <- Spp(mypath, "Summer flounder", cruise="AprthruOct", outdir="~/output/sasr")
-#sppdata <- Spp(mypath, "Tautog", outdir="~/output/sasr")
-#sppdata <- Spp(mypath, "Weakfish", cruise="AugOct", outdir="~/output/sasr")
-##sppdata <- Spp(mypath, "Lobster - F (GE53mm)")
-##sppdata <- Spp(mypath, "Lobster - M (GE53mm)")
-#Spp(mypath, "Atl croaker", cruise="AugOct", outdir=outdir)
-#sppdata <- lfd(mypath, "Summer flounder", cruise="AprthruOct", outdir="~/output/sasr")
-sppdata <- Spp(mypath, spp="Lobster - F (GE53mm)", outdir="~/output/sasr")
-sppdata <- Spp(mypath, "Lobster - F (GE53mm)", cruise="Spring", outdir="~/output/sasr")
-sppdata <- Spp(mypath, "Lobster - F (GE53mm)", cruise="Oct", outdir="~/output/sasr")
-sppdata <- lfd(mypath, spp="Lobster - F (GE53mm)", cruise="Spring", outdir="~/output/sasr")
-sppdata <- lfd(mypath, "Lobster - F (GE53mm)", cruise="Oct", outdir="~/output/sasr")
+#sppdata <- Spp(input, "Black drum", "INM", "Oct", outdir="~/output/sasr")
+#sppdata <- Spp(input, "Scup", outdir=outdir) #"INM", "AugOct"
+#sppdata <- Spp(input, spp="Spot", area="INM", cruise="AugOct", outdir=outdir)
+#sppdata <- Spp(input, "Summer flounder", outdir="~/output/sasr")
+#sppdata <- Spp(input, "Summer flounder", cruise="AprthruOct", outdir="~/output/sasr")
+#sppdata <- Spp(input, "Tautog", outdir="~/output/sasr")
+#sppdata <- Spp(input, "Weakfish", cruise="AugOct", outdir="~/output/sasr")
+##sppdata <- Spp(input, "Lobster - F (GE53mm)")
+##sppdata <- Spp(input, "Lobster - M (GE53mm)")
+#Spp(input, "Atl croaker", cruise="AugOct", outdir=outdir)
+#sppdata <- lfd(input, "Summer flounder", cruise="AprthruOct", outdir="~/output/sasr")
+sppdata <- Spp(input, spp="Lobster - F (GE53mm)", outdir="~/output/sasr")
+sppdata <- Spp(input, "Lobster - F (GE53mm)", cruise="Spring", outdir="~/output/sasr")
+sppdata <- Spp(input, "Lobster - F (GE53mm)", cruise="Oct", outdir="~/output/sasr")
+sppdata <- lfd(input, spp="Lobster - F (GE53mm)", cruise="Spring", outdir="~/output/sasr")
+sppdata <- lfd(input, "Lobster - F (GE53mm)", cruise="Oct", outdir="~/output/sasr")
 
-sppdata <- Spp(mypath, "Lobster - M (GE53mm)", outdir="~/output/sasr")
-sppdata <- Spp(mypath, "Lobster - M (GE53mm)", cruise="Spring", outdir="~/output/sasr")
-sppdata <- Spp(mypath, "Lobster - M (GE53mm)", cruise="Oct", outdir="~/output/sasr")
-sppdata <- lfd(mypath, "Lobster - M (GE53mm)", cruise="Spring", outdir="~/output/sasr")
-sppdata <- lfd(mypath, "Lobster - M (GE53mm)", cruise="Oct", outdir="~/output/sasr")
+sppdata <- Spp(input, "Lobster - M (GE53mm)", outdir="~/output/sasr")
+sppdata <- Spp(input, "Lobster - M (GE53mm)", cruise="Spring", outdir="~/output/sasr")
+sppdata <- Spp(input, "Lobster - M (GE53mm)", cruise="Oct", outdir="~/output/sasr")
+sppdata <- lfd(input, "Lobster - M (GE53mm)", cruise="Spring", outdir="~/output/sasr")
+sppdata <- lfd(input, "Lobster - M (GE53mm)", cruise="Oct", outdir="~/output/sasr")
 
