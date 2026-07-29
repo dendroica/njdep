a82a9a259cb215e638a800b45835d0e8d81387b2
dendroica <galaxycoff@gmail.com>
2026-07-29 16:14:07 -0400

    blue crab

--- mrip_inhouse/dev.R
+++
@@ -1,6 +0,0 @@
-library(devtools)
-dev_mode(on=T)
-#devtools::install_github("dendroica/mriptools@data")
-detach("package:mriptools", unload = TRUE)
-devtools::update_packages("mriptools")
-dev_mode(on = FALSE)
