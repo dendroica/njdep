66bb20c1155a0c581e6bb64292f393972d203147
dendroica <galaxycoff@gmail.com>
2026-08-03 13:40:17 -0400

    lint

--- pseg/3-query.R
+++ pseg/3-query.R
@@ -1,46 +1,46 @@
-# scan for sturgeon (Atlantic and short nose) and turtle (all species) records and join only those records
+# scan for sturgeon (Atlantic and short nose) and spp (all species) records and join only those records
 # source("2-pseg_datafix.R")
 library(sf)
 sf::sf_use_s2(FALSE)
 library(dplyr)
-sppofinterest <- "BLACK DRUM"
+spp_of_interest <- "BLACK DRUM"
 
 root <- Sys.getenv("VPATH")
-out <- root
+out <- Sys.getenv("VOUT")
 path <- file.path(root, "pseg/PSEG_Data_compiled.RData")
 load(path)
 load(file.path(root, "pseg/pseg_dict.RData"))
-beachlocs <- read.csv(file.path(root, "pseg/PSEG 2025 Beach Seine Locations.csv"))
-trawllocs <- read.csv(file.path(root, "pseg/PSEG Bottom Trawl Locations.csv"))[, 1:3]
+beach_locs <- read.csv(file.path(root, "pseg/PSEG 2025 Beach Seine Locations.csv"))
+trawl_locs <- read.csv(file.path(root, "pseg/PSEG Bottom Trawl Locations.csv"))[, 1:3]
 strata <- read_sf(file.path(root, "pseg/pseg_strata.gpkg"))
 #########
 
-fullnames <- gsub("(Level [0-9]_[A-Za-z ]* [0-9]{4}).*", "\\1", names(dups))
-gears <- unique(sapply(fullnames, function(y) gsub("Level [0-9]_([A-Za-z ]*) [0-9]{4}.*", "\\1", y)))
+data_names <- gsub("(Level [0-9]_[A-Za-z ]* [0-9]{4}).*", "\\1", names(pseg))
+gears <- unique(sapply(data_names, function(y) gsub("Level [0-9]_([A-Za-z ]*) [0-9]{4}.*", "\\1", y)))
 samples <- sapply(gears, function(x) {
-  sum(sapply(dups[which(grepl(paste0("Level 1_", x), fullnames))], nrow))
+  sum(sapply(pseg[which(grepl(paste0("Level 1_", x), data_names))], nrow))
 })
 
-getspp <- function(x) {
+get_spp <- function(x) {
   if ("COMMON_NAME" %in% names(x)) {
-    x <- x[grep(sppofinterest, x$COMMON_NAME), ] # "STRIPED BASS STURGEON|TURTLE
+    x <- x[grep(spp_of_interest, x$COMMON_NAME), ] # "STRIPED BASS STURGEON|spp
   } else {
     x <- data.frame()
   }
   return(x)
 }
-turtle <- lapply(dups, getspp)
-turtle <- turtle[sapply(turtle, nrow) > 0]
+spp <- lapply(pseg, get_spp)
+spp <- spp[sapply(spp, nrow) > 0]
 
-coords <- apply(beachlocs[, 2:3], 2, strsplit, " ")
+coords <- apply(beach_locs[, 2:3], 2, strsplit, " ")
 deg <- apply(as.data.frame(lapply(coords, function(x) unlist(lapply(x, "[[", 1)))), 2, as.numeric)
 min <- apply(as.data.frame(lapply(coords, function(x) unlist(lapply(x, "[[", 2)))), 2, as.numeric) / 60
-beachlocs <- as.data.frame(deg + min)
-beachlocs$sta <- 1:nrow(beachlocs)
-beachlocs$lng <- beachlocs$lng * -1
+beach_locs <- as.data.frame(deg + min)
+beach_locs$sta <- 1:nrow(beach_locs)
+beach_locs$lng <- beach_locs$lng * -1
 
 df_sf <- st_as_sf(
-  x = trawllocs,
+  x = trawl_locs,
   coords = c("Easting", "Northing"),
   crs = "+proj=utm +zone=18 +datum=NAD83"
 )
@@ -59,7 +59,7 @@ lvl2 <- Map(function(x, y) {
   fish <- x$COMMON_NAME
   print(y)
   if (gsub("(Level [0-9])_[A-Za-z ]* [0-9]{4}.*", "\\1", y) == "Level 2") {
-    lvl1 <- fullnames[which(grepl(paste0("Level 1_", gear), fullnames))]
+    lvl1 <- data_names[which(grepl(paste0("Level 1_", gear), data_names))]
     if (any(as.integer(gsub("Level [0-9]_[A-Za-z ]* ([0-9]{4}).*", "\\1", lvl1)) == yr)) {
       index <- which(as.integer(gsub("Level [0-9]_[A-Za-z ]* ([0-9]{4}).*", "\\1", lvl1)) == yr)
     } else {
@@ -67,69 +67,71 @@ lvl2 <- Map(function(x, y) {
     }
     level1 <- lvl1[index]
     print(nrow(x))
-    ref <- dups[[which(fullnames == level1)]]
+    ref <- pseg[[which(data_names == level1)]]
     names(ref) <- toupper(names(ref))
-    
-    out <- merge(x, ref, by = names(x)[names(x) %in% c("SAMPLE_ID", "SAMPLNO", "DBO_ES_ST_SAMPLE_SAMPLE_ID")], all=T)
-    #if ("SAMPLNO" %in% names(x)) { #this is the longer identifier, use it where you have it
+
+    out <- merge(x, ref, by = names(x)[names(x) %in% c("SAMPLE_ID", "SAMPLNO", "DBO_ES_ST_SAMPLE_SAMPLE_ID")], all = T)
+    # if ("SAMPLNO" %in% names(x)) { #this is the longer identifier, use it where you have it
     #  out <- merge(x, ref, by = "SAMPLNO", all = T)
-    #} else if (toupper("dbo_es_st_sample_sample_id") %in% names(ref)) {
+    # } else if (toupper("dbo_es_st_sample_sample_id") %in% names(ref)) {
     #  out <- merge(x, ref, by.x = toupper("dbo_es_st_sample_sample_id"), by.y = toupper("dbo_es_st_sample_sample_id"), all = T)
-      #out$SAMPLNO <- out$DBO_ES_ST_SAMPLE_SAMPLE_ID
-    #}
+    # out$SAMPLNO <- out$DBO_ES_ST_SAMPLE_SAMPLE_ID
+    # }
     out$GEAR_TYPE <- gear
-    
+
     out[is.na(out$FILENAME.x), ]$FILENAME.x <- out[is.na(out$FILENAME.x), ]$FILENAME.y
     out$filename <- out$FILENAME.x
     out$FILENAME.x <- NULL
     out$FILENAME.y <- NULL
     zerofill <- out[is.na(out$COMMON_NAME), ]
     zerofill$TOTAL_COUNTED <- 0
-    zerofill$COMMON_NAME <- sppofinterest
+    zerofill$COMMON_NAME <- spp_of_interest
     obs <- out[!is.na(out$COMMON_NAME), ]
-    #obs$COMMON_NAME <- fish #why do we need this?
+    # obs$COMMON_NAME <- fish #why do we need this?
     out <- rbind(obs, zerofill)
-    if("GEAR" %in% names(out)) {
+    if ("GEAR" %in% names(out)) {
       out$GEARTYPE <- datatabs[[1]]$Gear_Code_lookup$Definition[match(as.character(out$GEAR), datatabs[[1]]$Gear_Code_lookup$Value)]
     }
-    
-    if ("GRID" %in% names(out)) { #Bottom Trawl, Ichthyoplankton, Pelagic Trawl
-      out <- merge(out, joined_points, by.x = "GRID", by.y = "GRID_ID", all.x = T) #I'm still not 100% these are the same "grids"
+
+    if ("GRID" %in% names(out)) { # Bottom Trawl, Ichthyoplankton, Pelagic Trawl
+      out <- merge(out, joined_points, by.x = "GRID", by.y = "GRID_ID", all.x = T) # I'm still not 100% these are the same "grids"
       out$GRID <- NULL
-      out$strata <- NULL #this is the one from joined_points, might need where strata is missing? but this is mostly to get area
-      #out <- st_drop_geometry(out) do you need to do this?
+      out$strata <- NULL # this is the one from joined_points, might need where strata is missing? but this is mostly to get area
+      # out <- st_drop_geometry(out) do you need to do this?
     } else if (gear == "Beach Seine") {
-        out$LOC_CODE <- as.numeric(gsub("[BWSbws ]*([0-9]+)", "\\1", out$SITE_ID))
-        out <- merge(out, beachlocs, by.x = "LOC_CODE", by.y = "sta", all.x = T)
-        out$LOC_CODE <- NULL
+      out$LOC_CODE <- as.numeric(gsub("[BWSbws ]*([0-9]+)", "\\1", out$SITE_ID))
+      out <- merge(out, beach_locs, by.x = "LOC_CODE", by.y = "sta", all.x = T)
+      out$LOC_CODE <- NULL
     }
-    
+
     if ("START_LAT_DD" %in% names(out) & !"lat" %in% names(out)) {
       out[is.na(out$START_LAT_SS), ]$START_LAT_SS <- 0
       out[is.na(out$START_LON_SS), ]$START_LON_SS <- 0
       out$lat <- out$START_LAT_DD + (out$START_LAT_MM + as.numeric(out$START_LAT_SS)) / 60
       out$lng <- (out$START_LON_DD + (out$START_LON_MM + as.numeric(out$START_LON_SS)) / 60) * -1
     }
-    
-    if(gear=="Bottom Trawl" & !any(names(out)=="STRATA")) {
-      matchup <- out[!is.na(out$lat),] #notice this is all that gets kept, should records w/o lat get folded back in?
-      matchup <- st_as_sf(matchup, coords=c("lng", "lat"), crs = "+proj=longlat +datum=WGS84")
+
+    if (gear == "Bottom Trawl" & !any(names(out) == "STRATA")) {
+      matchup <- out[!is.na(out$lat), ] # notice this is all that gets kept, should records w/o lat get folded back in?
+      matchup <- st_as_sf(matchup, coords = c("lng", "lat"), crs = "+proj=longlat +datum=WGS84")
       joinedpoints <- st_join(matchup, strata, join = st_intersects)
-      names(joinedpoints)[names(joinedpoints)=="strata"] <- "STRATA"
+      names(joinedpoints)[names(joinedpoints) == "strata"] <- "STRATA"
       out <- st_drop_geometry(joinedpoints)
     }
-    
-    environ <- out[!duplicated(out$SAMPLE_ID),]
+
+    environ <- out[!duplicated(out$SAMPLE_ID), ]
     environ$TOTAL_COUNTED <- NULL
     out$TOTAL_COUNTED <- as.integer(out$TOTAL_COUNTED)
-    catch <- out %>% select(SAMPLE_ID, COMMON_NAME, TOTAL_COUNTED) %>% group_by(SAMPLE_ID, COMMON_NAME) %>% #I broke this, it must be changing this here...check type is char?
-      summarise(TOTAL_COUNTED = sum(TOTAL_COUNTED)) #this sums up over fish groups
+    catch <- out %>%
+      select(SAMPLE_ID, COMMON_NAME, TOTAL_COUNTED) %>%
+      group_by(SAMPLE_ID, COMMON_NAME) %>% # I broke this, it must be changing this here...check type is char?
+      summarise(TOTAL_COUNTED = sum(TOTAL_COUNTED)) # this sums up over fish groups
     out <- merge(environ, catch)
   } else {
     out <- data.frame()
   }
   return(out)
-}, turtle, names(turtle))
+}, spp, names(spp))
 lvl2 <- lvl2[sapply(lvl2, nrow) > 0]
 
 lvl3 <- Map(function(x, y) {
@@ -143,18 +145,18 @@ lvl3 <- Map(function(x, y) {
     if (any(meldin)) {
       # print(meldin)
       lvl3 <- lvl2[[which(meldin)]]
-      #names(lvl3) <- toupper(names(lvl3))
+      # names(lvl3) <- toupper(names(lvl3))
       commonfields <- names(x)[names(x) %in% names(lvl3)]
       commonfields <- commonfields[!commonfields %in% c("FILENAME", "NODC")]
       # print(nrow(x))
       out <- merge(x, lvl3, by = commonfields, all.x = T)
     }
-    
-    #if (!"SAMPLNO" %in% names(out)) {
+
+    # if (!"SAMPLNO" %in% names(out)) {
     #  out$SAMPLNO <- out$DBO_ES_ST_SAMPLE_SAMPLE_ID
     #  out$DBO_ES_ST_SAMPLE_SAMPLE_ID <- NULL
-    #}
-    
+    # }
+
     if (!"CNT_LVL3" %in% names(out)) {
       out$CNT_LVL3 <- 1
     }
@@ -175,7 +177,7 @@ lvl3 <- Map(function(x, y) {
     out <- data.frame()
   }
   return(out)
-}, turtle, names(turtle))
+}, spp, names(spp))
 lvl3 <- lvl3[sapply(lvl3, nrow) > 0]
 # 95081009100026420
 # [1] "Level 2_Beach Seine 1995_V:/Marine Fish/Jeff B/PSEG data/PSEG/rawdata/PSEG/Beach Seine/Beach Seine 1995.accdb" GPS_GROUP_ID (do same extract as next)
@@ -215,18 +217,19 @@ lvl3 <- lvl3[sapply(lvl3, nrow) > 0]
 datatabs[[1]]$GPS_Group_ID_lookup <- datatabs[[1]]$GPS_Group_ID_lookup[-which(datatabs[[1]]$GPS_Group_ID_lookup$Value %in% c("BZ- 1", "CP", "CRL", "EAST INT", "LOW OFFMAL", "UPPERDITCH", "UPPERDUCK", "WEST INT", "SNC1A", "glp5", "TEST", "crl5", "LOW  LEFT")), ]
 
 # who knows if there are actually lookup values hiding in site_id?
-findcode <- function(x) {
-  x[which(apply(x, 1, function(y) any(grepl("SNC1A", y)))), ]
-}
-# checkcode <- lapply(dups, findcode)
+# findcode <- function(x) {
+#  x[which(apply(x, 1, function(y) any(grepl("SNC1A", y)))), ]
+# }
+# checkcode <- lapply(pseg, findcode)
 # checkcode <- checkcode[sapply(checkcode, nrow)>0]
-datatabs[[1]]$GPS_Group_ID_lookup[datatabs[[1]]$GPS_Group_ID_lookup$Value == "SNGS", ]$Value <- datatabs[[1]]$GPS_Group_ID_lookup[datatabs[[1]]$GPS_Group_ID_lookup$Value == "SNGS", ]$site_id
+sngs <- which(datatabs[[1]]$GPS_Group_ID_lookup$Value == "SNGS")
+datatabs[[1]]$GPS_Group_ID_lookup[sngs, ]$Value <- datatabs[[1]]$GPS_Group_ID_lookup[sngs, ]$site_id
 datatabs[[1]]$GPS_Group_ID_lookup$Value <- gsub(" ", "", toupper(datatabs[[1]]$GPS_Group_ID_lookup$Value))
 datatabs[[1]]$Unit_Measure_Weight_lookup$Value <- as.integer(datatabs[[1]]$Unit_Measure_Weight_lookup$Value)
 
 # pull relevant info (e.g. gear, date, location, species, size)
 # This way, we only need to know the names for the important fields, and it should be easy enough to standardize the names before joining them.
-#lvl2 <- lapply(lvl2, function(x) {
+# lvl2 <- lapply(lvl2, function(x) {
 #  x <- x[, names(x)[names(x) %in%
 #                      c(toupper(c(
 #                        "common_name", "SAMPLNO", "gear", "gear_type", "geartype",
@@ -244,7 +247,7 @@ datatabs[[1]]$Unit_Measure_Weight_lookup$Value <- as.integer(datatabs[[1]]$Unit_
 #                        "filename", "DUR", "STRATA"
 #                      )), "FILENAME.x", "FILENAME.y", "filename", "siteid", "lat", "lng")]]
 #  return(x)
-#})
+# })
 
 # fields I made: gear_type, geartype, site, filename
 if (any(grepl("Level 2_Impingement 1995", names(lvl2)))) {
@@ -277,54 +280,56 @@ lvl2[[2]]$gps_group_indicator_id <- datatabs[[1]]$GPS_Group_ID_lookup$gps_group_
 
 # lvl2[[3]][is.na(lvl2[[3]]$CNT_LVL3), ]$CNT_LVL3 <- lvl2[[3]][is.na(lvl2[[3]]$CNT_LVL3), ]$TOTAL_COUNTED
 # lvl2[[3]]$count <- lvl2[[3]]$CNT_LVL3
-# Maybe also total number of stations conducted for the year (whether sturgeon/turtle were caught or not), to get an estimate of overall interaction frequency
+# Maybe also total number of stations conducted for the year (whether sturgeon/spp were caught or not), to get an estimate of overall interaction frequency
 # [[1]]$Weight_Measurement_Code_lookup
 
 # "MAXIMUM_WEIGHT", "MINIMUM_WEIGHT",
 lvl2s <- lapply(lvl2, function(x) {
-  x <- x[, names(x)[!names(x) %in% c( "END_LAT_DD", "END_LAT_MM", "END_LON_DD",
-                                      "END_LON_MM"
+  x <- x[, names(x)[!names(x) %in% c(
+    "END_LAT_DD", "END_LAT_MM", "END_LON_DD",
+    "END_LON_MM"
   )]]
   return(x)
 })
 
-outdata <- lapply(1:length(lvl2s), function(a) {
-  if((a+1) < length(lvl2s) && lvl2s[[a+1]]$GEAR_TYPE[1] == lvl2s[[a]]$GEAR_TYPE[1]) {
-    b <- bind_rows(lvl2s[[a]], lvl2s[[a+1]])
-    b <- b[,!grepl("y",names(b))]
-    #b$COLLECTION_DATE <- format(b$COLLECTION_DATE, "%Y-%m-%d %H:%M:%S")
+out_data <- lapply(1:length(lvl2s), function(a) {
+  if ((a + 1) < length(lvl2s) && lvl2s[[a + 1]]$GEAR_TYPE[1] == lvl2s[[a]]$GEAR_TYPE[1]) {
+    b <- bind_rows(lvl2s[[a]], lvl2s[[a + 1]])
+    b <- b[, !grepl("y", names(b))]
+    # b$COLLECTION_DATE <- format(b$COLLECTION_DATE, "%Y-%m-%d %H:%M:%S")
     lvl2data <- b[order(b$DATE), ]
-  } else if (a-1 >0 && lvl2s[[a-1]]$GEAR_TYPE[1] == lvl2s[[a]]$GEAR_TYPE[1]) {
+  } else if (a - 1 > 0 && lvl2s[[a - 1]]$GEAR_TYPE[1] == lvl2s[[a]]$GEAR_TYPE[1]) {
     lvl2data <- data.frame()
   } else {
     b <- lvl2s[[a]]
-    b <- b[,!grepl("y",names(b))]
-    #b$COLLECTION_DATE <- format(b$COLLECTION_DATE, "%Y-%m-%d %H:%M:%S")
+    b <- b[, !grepl("y", names(b))]
+    # b$COLLECTION_DATE <- format(b$COLLECTION_DATE, "%Y-%m-%d %H:%M:%S")
     lvl2data <- b[order(b$DATE), ]
   }
-return(lvl2data)})
-outdata <- outdata[sapply(outdata, nrow) > 0]
-lapply(outdata, function(d) {
+  return(lvl2data)
+})
+out_data <- out_data[sapply(out_data, nrow) > 0]
+lapply(out_data, function(d) {
   d <- d[, colSums(!is.na(d)) > 0]
-  write.csv(d, file = file.path(out, paste0(sppofinterest, "-catch_", d$GEAR_TYPE[1], ".csv")), row.names = F)
+  write.csv(d, file = file.path(out, paste0(spp_of_interest, "-catch_", d$GEAR_TYPE[1], ".csv")), row.names = F)
 })
 
-#outdata <- bind_rows(lvl2s)
-#outdata <- outdata[, c(
+# out_data <- bind_rows(lvl2s)
+# out_data <- out_data[, c(
 #  "GEAR_TYPE", "GEARTYPE", "COLLECTION_DATE", "SAMPLNO", "DUR", "STRATA",
 #  "siteid", "lat", "lng", "COMMON_NAME", "TOTAL_COUNTED",
 #  # "INDIVIDUAL_LENGTH", "LENGTH_MEASURE_TECHNIQUE_CODE",
 #  # "TOTAL_WEIGHT", "wt",
 #  "filename"
-#)]
-#lvl2data <- outdata[order(outdata$GEAR_TYPE, outdata$COLLECTION_DATE), ]
-#lvl2data$COLLECTION_DATE <- format(lvl2data$COLLECTION_DATE, "%Y-%m-%d %H:%M:%S")
+# )]
+# lvl2data <- out_data[order(out_data$GEAR_TYPE, out_data$COLLECTION_DATE), ]
+# lvl2data$COLLECTION_DATE <- format(lvl2data$COLLECTION_DATE, "%Y-%m-%d %H:%M:%S")
 # can you match ichthyoplankton with grid? pelagic trawl?
 # write.csv(lvl2data, "~/output/stripedbass_catch-ALL.csv", row.names = F)
 
-outdata3 <- lapply(lvl3, function(x) {
+out_data_lvl3 <- lapply(lvl3, function(x) {
   x <- x[, names(x)[names(x) %in% c(
-    "SAMPLE_ID", "CNT_LVL3", 
+    "SAMPLE_ID", "CNT_LVL3",
     "COMMON_NAME", "expand", "filename",
     "expandct", "GEAR_TYPE",
     "TOTAL_WEIGHT", "LENGTH", "wt", "TOTAL_COUNTED",
@@ -332,13 +337,13 @@ outdata3 <- lapply(lvl3, function(x) {
   )]]
   return(x)
 })
-outdata3 <- bind_rows(outdata3)
-#outdata[is.na(outdata$LENGTH), ]$LENGTH <- outdata[is.na(outdata$LENGTH), ]$INDIVIDUAL_LENGTH
-#outdata$INDIVIDUAL_LENGTH <- NULL
+out_data_lvl3 <- bind_rows(out_data_lvl3)
+# out_data[is.na(out_data$LENGTH), ]$LENGTH <- out_data[is.na(out_data$LENGTH), ]$INDIVIDUAL_LENGTH
+# out_data$INDIVIDUAL_LENGTH <- NULL
 
-mismatch <- outdata3[outdata3$expand < 1 & !is.na(outdata3$expand), ]
-good <- outdata3[outdata3$expand >= 1 & !is.na(outdata3$expand), ]
-write.csv(outdata3, file.path(out, paste0(sppofinterest,"_length.csv")), row.names = F)
+mismatch <- out_data_lvl3[out_data_lvl3$expand < 1 & !is.na(out_data_lvl3$expand), ]
+good <- out_data_lvl3[out_data_lvl3$expand >= 1 & !is.na(out_data_lvl3$expand), ]
+write.csv(out_data_lvl3, file.path(out, paste0(spp_of_interest, "_length.csv")), row.names = F)
 
 # EDA
 summary_data <- mismatch %>%
@@ -348,6 +353,6 @@ summary_data <- mismatch %>%
 summary_data$expand <- summary_data$TOTAL_COUNTED / summary_data$ct
 # out$expand <- summary_data$expand[match(out$SAMPLNO, summary_data$SAMPLNO)]
 # out$expandct <- out$CNT_LVL3 * out$expand
-#"000035144" "000049376" "000056791" "000066069" "000095240" "000095251" "000095260" "000096096"
+# "000035144" "000049376" "000056791" "000066069" "000095240" "000095251" "000095260" "000096096"
 
-#to do: make sure no NA sample ids, check to see if sample IDs are unique ACROSS survey types? (remember fixed or created ids too)
+# to do: make sure no NA sample ids, check to see if sample IDs are unique ACROSS survey types? (remember fixed or created ids too)
