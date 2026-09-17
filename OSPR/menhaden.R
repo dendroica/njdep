c37d96cc5b29d60e6bc4dde1310c6296596da217
Jessica Gorzo <galaxycoff@gmail.com>
2026-09-17 14:28:48 -0400

    menhaden script

--- OSPR/menhaden.R
+++ OSPR/menhaden.R
@@ -4,10 +4,9 @@ menhaden$Site.Name[menhaden$Site.Name %in% c("Berkeley Island Park", "Berkeley I
 menhaden$Site.Name[menhaden$Site.Name %in% c("Cattus Island Park Beach")] <- "Cattus Isl Control"
 menhaden$Site.Name[menhaden$Site.Name %in% c("Cattus Island Park Point")] <- "Cattus Isl Point"
 
-ospr <- read.csv(file.path(Sys.getenv("FILEPATH"), "data/seining locations_CORRECTED.csv"))
-ospr$Name[6] <- "Browns Woods Beach"
-ospr$Name[15] <- "East Green Street Point"
-ospr$Name[11] <- "Barnegat Public Beach"
-ospr <- ospr %>% 
+seine <- read.csv(file.path(Sys.getenv("FILEPATH"), "data/seining locations_CORRECTED.csv"))
+seine$Name[6] <- "Browns Woods Beach"
+seine$Name[15] <- "East Green Street Point"
+seine$Name[11] <- "Barnegat Public Beach"
+seine <- seine %>% 
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326)
-#ospr <- st_transform(ospr, crs=st_crs(ensp_nest))
