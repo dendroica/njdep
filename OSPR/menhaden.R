5b7b60f3cda333de0527992f5f66d7418a288097
dendroica <galaxycoff@gmail.com>
2026-09-18 08:55:05 -0400

    menhaden data

--- OSPR/menhaden.R
+++ OSPR/menhaden.R
@@ -3,6 +3,11 @@ menhaden <- read.csv(file.path(Sys.getenv("FILEPATH"), "data/John JJ Egan bunker
 menhaden$Site.Name[menhaden$Site.Name %in% c("Berkeley Island Park", "Berkeley Island Park Profile")] <- "Berkeley Island Beach"
 menhaden$Site.Name[menhaden$Site.Name %in% c("Cattus Island Park Beach")] <- "Cattus Isl Control"
 menhaden$Site.Name[menhaden$Site.Name %in% c("Cattus Island Park Point")] <- "Cattus Isl Point"
+menhaden$Site.Name[menhaden$Site.Name %in% c("Iowa Point")] <- "Iowa Cove"
+menhaden$Site.Name[menhaden$Site.Name %in% c("East Green Street Cove")] <- "East Green Street Point"
+menhaden$Site.Name[menhaden$Site.Name %in% c("Ocean Gate")] <- "Angelsea Ave"
+menhaden$Site.Name[menhaden$Site.Name %in% c("Lavallette")] <- "Brooklyn Ave"
+menhaden$Site.Name[menhaden$Site.Name %in% c("Great Bay Blvd. (1st bridge)")] <- "First Bridge Beach"
 
 seine <- read.csv(file.path(Sys.getenv("FILEPATH"), "data/seining locations_CORRECTED.csv"))
 seine$Name[6] <- "Browns Woods Beach"
